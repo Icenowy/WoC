45bd9cd9913b467a453a01c9960e38ce212b6f9c
Icenowy Zheng <icenowy@outlook.com>
2015-12-10 00:00:35 +0800

    add "MOV" ins

--- vm/vcpu.c
+++ vm/vcpu.c
@@ -104,7 +104,12 @@ _Bool vcpu_execute (struct vcpu *cpu, uint32_t *mem, uint32_t *mem_color)
 			TWIREG_INS_START
 			*preg1 >>= *preg2;
 			TWIREG_INS_END
-			case 0x00010000:
+		case 0x000B0000:
+			// MOV REG REG
+			TWIREG_INS_START
+			*preg1 = *preg2;
+			TWIREG_INS_END
+		case 0x00010000:
 			switch (instruction & 0x0000FF00) {
 			case 0x0000000:
 				goto normal_exit;
